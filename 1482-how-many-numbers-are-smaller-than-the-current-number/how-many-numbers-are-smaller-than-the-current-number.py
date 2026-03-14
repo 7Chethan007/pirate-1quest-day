9e34e7a5bd76ebe9c7831b6b4650c4b7f9ce8605
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-14 22:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (56.66%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,14 +1,35 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * n
+        # n = len(nums)
+        # ans = [0] * n
 
-        count = 0
-        for i in range(n):
-            for j in range(n):
-                if nums[j] < nums[i]:
-                    count += 1
-            ans[i] = count
-            count = 0
+        # count = 0
+        # for i in range(n):
+        #     for j in range(n):
+        #         if nums[j] < nums[i]:
+        #             count += 1
+        #     ans[i] = count
+        #     count = 0
         
-        return ans
+        # return ans
+        # # T : O(n^2) && S : O(n)
+
+        # Count Sort && HASH MAp
+         # Step 1: frequency bucket
+        bucket = [0] * 102
+        
+        for num in nums:
+            bucket[num] += 1
+        
+        # Step 2: prefix sum (how many numbers ≤ i)
+        for i in range(1, 102):
+            bucket[i] += bucket[i - 1]
+        
+        # Step 3: build result
+        res = [0] * len(nums)
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                res[i] = bucket[nums[i] - 1]
+        
+        return res
