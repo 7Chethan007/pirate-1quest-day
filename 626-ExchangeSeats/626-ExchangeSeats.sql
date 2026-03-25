cf1b214baba02ca686f4ef132ec145e09fb9c103
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:04:50 +0530

    Create 626-ExchangeSeats

---
+++ 626-ExchangeSeats/626-ExchangeSeats.sql
@@ -0,0 +1,11 @@
+-- Last updated: 3/25/2026, 9:04:48 AM
+# Write your MySQL query statement below
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT COUNT(*) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
