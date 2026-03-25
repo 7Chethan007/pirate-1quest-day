6f87158708acd9ce8297eeedccb843fee63c5aee
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:02:49 +0530

    Create 2761-ArrayReduceTransformation

---
+++ 2761-ArrayReduceTransformation/2761-ArrayReduceTransformation.js
@@ -0,0 +1,14 @@
+// Last updated: 3/25/2026, 9:02:46 AM
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    for (let i = 0; i < nums.length; i++) {
+        val = fn(val, nums[i]);
+    }
+    return val;
+};
