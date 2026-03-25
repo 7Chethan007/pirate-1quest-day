5bce604915e052c0bde8a66cbf6b97731733ec2c
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:02:38 +0530

    Create 2805-ArrayWrapper

---
+++ 2805-ArrayWrapper/2805-ArrayWrapper.js
@@ -0,0 +1,31 @@
+// Last updated: 3/25/2026, 9:02:36 AM
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((sum, n) => sum + n, 0);
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(",")}]`;
+}
+
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
