eaa17c220cfb5d21bf11ad33f78b608a13a28162
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:02:32 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 3/25/2026, 9:02:31 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * Example:
+ * argumentsLength(1, 2, 3); // 3
+ */
