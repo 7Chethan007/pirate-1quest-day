68bdad603520480b4ec922120318ef514c1589e6
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:06:22 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,61 @@
+// Last updated: 3/25/2026, 9:06:20 AM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0; // count
+        int el; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (el == nums[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //declaring a map:
+        // map<int, int> mpp;
+        // //storing the elements with its occurnce:
+        // for (int i = 0; i < n; i++) {
+        //     mpp[nums[i]]++;
+        // }
+        // //searching for the majority element:
+        // for (auto it : mpp) {
+        //     if (it.second > (n / 2)) {
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+    }
+};
