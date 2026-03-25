5ddb133b7c2527ad1ed419d183f0550fe3596f9b
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:02:45 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,27 @@
+// Last updated: 3/25/2026, 9:02:44 AM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+    return {
+        increment: function(){
+            return ++current;
+        },
+        decrement: function() {
+            return --current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
