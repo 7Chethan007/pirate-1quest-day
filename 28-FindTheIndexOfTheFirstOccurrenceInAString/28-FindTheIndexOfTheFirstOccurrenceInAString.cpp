7e5a2bf42d5a4b4a26b7350432b6f948fa9fb8ba
Chethan M N <116062837+7Chethan007@users.noreply.github.com>
2026-03-25 09:07:50 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 3/25/2026, 9:07:48 AM
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        if (m > n) return -1;
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substr(i, m) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
